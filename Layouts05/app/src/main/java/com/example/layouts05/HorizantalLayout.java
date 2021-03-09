9864e172f637b96fa7c2ad6297087bf730447c18
usamaimran <xamimran8991@gmail.com>
2021-03-09 07:00:12 -0800

    Setting up  Layouts

--- Layouts05/app/src/main/java/com/example/layouts05/HorizantalLayout.java
+++ Layouts05/app/src/main/java/com/example/layouts05/HorizantalLayout.java
@@ -2,7 +2,9 @@ package com.example.layouts05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HorizantalLayout extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class HorizantalLayout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horizantal_layout);
+    }
+    public void moveToActivity1(View view) {
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
--- Layouts05/app/src/main/java/com/example/layouts05/MainActivity.java
+++ Layouts05/app/src/main/java/com/example/layouts05/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.layouts05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+    }
+    public void moveToActivity2(View view) {
+        Intent intent=new Intent(this,HorizantalLayout.class);
+        startActivity(intent);
     }
 }
