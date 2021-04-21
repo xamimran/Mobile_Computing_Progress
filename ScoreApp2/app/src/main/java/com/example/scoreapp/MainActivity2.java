9ec9f2f2fc0e6d41dbfe8946dc289579447ceecd
usamaimran <xamimran8991@gmail.com>
2021-04-21 06:50:33 -0800

    MOving TO Other Activity

--- ScoreApp2/app/src/main/java/com/example/scoreapp/MainActivity2.java
+++ ScoreApp2/app/src/main/java/com/example/scoreapp/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.scoreapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void onClick(View v)
+    {
+        // TODO Auto-generated method stub
+        Intent i = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(i);
+
     }
 }
